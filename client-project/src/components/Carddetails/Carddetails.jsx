f1b38da656d78e5a8c3fbb5e246523901c228288
Kenny Jewett <kennethrjewett@gmail.com>
2023-05-19 12:55:19 -0500

    Imported Api data and added reduc slice import to carddetails

--- client-project/src/components/Carddetails/Carddetails.jsx
+++ client-project/src/components/Carddetails/Carddetails.jsx
@@ -1,54 +1,76 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from '../Card/Card.jsx'
+import { useSelector } from 'react-redux'
 
 const Carddetails = () => {
 
-  const productData = [{
-		image : "https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder-1024x1024.png",
-    	companyName : "companyName",
-    	productName : "productName",
-    	productType : "productType",
-    	description : "description",
-    	price : "price",
-    	quantity : "quantity"    
-    },
-	{
-		image : "https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder-1024x1024.png",
-		companyName : "companyName",
-		productName : "productName",
-		productType : "productType",
-		description : "description",
-		price : "price",
-		quantity : "quantity"	
-	},
-	{
-		image : "https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder-1024x1024.png",
-		companyName : "companyName",
-		productName : "productName",
-		productType : "productType",
-		description : "description",
-		price : "price",
-		quantity : "quantity"	
-	},
-	{
-		image : "https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder-1024x1024.png",
-		companyName : "companyName",
-		productName : "productName",
-		productType : "productType",
-		description : "description",
-		price : "price",
-		quantity : "quantity"	
-	}]
+	 const productData = useSelector(store => store.inventory.items);
+
+
+	// const [ productData, setProductData] = useState();
+
+	// const apiURL = "http://localhost:3070/api/products/"
+
+	// const getProduct = async () => {
+	// 	const url = apiURL;
+	// 	const res =  await fetch(url);
+	// 	const data = await res.json();
+
+	// 	setProductData(data)
+	// };
+
+	// useEffect(() => {getProduct(apiURL)}, []);
+
+
+
+
+
+//   const productData = [{
+// 		image : "https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder-1024x1024.png",
+//     	companyName : "companyName",
+//     	productName : "productName",
+//     	productType : "productType",
+//     	description : "description",
+//     	price : "price",
+//     	quantity : "quantity"    
+//     },
+// 	{
+// 		image : "https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder-1024x1024.png",
+// 		companyName : "companyName",
+// 		productName : "productName",
+// 		productType : "productType",
+// 		description : "description",
+// 		price : "price",
+// 		quantity : "quantity"	
+// 	},
+// 	{
+// 		image : "https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder-1024x1024.png",
+// 		companyName : "companyName",
+// 		productName : "productName",
+// 		productType : "productType",
+// 		description : "description",
+// 		price : "price",
+// 		quantity : "quantity"	
+// 	},
+// 	{
+// 		image : "https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder-1024x1024.png",
+// 		companyName : "companyName",
+// 		productName : "productName",
+// 		productType : "productType",
+// 		description : "description",
+// 		price : "price",
+// 		quantity : "quantity"	
+// 	}]
 
     const productList = productData?.map((product, id) =>
 		(<Card
-			image={product.image} 
-			company={product.companyName}
+			image={"https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder-1024x1024.png"} 
+			company={product.productCompany}
 			product={product.productName}
 			productType={product.productType}
-			description={product.description}
-			price={product.price}
-			quantity={product.quantity}
+			description={product.productDescription}
+			price={product.productPrice}
+			quantity={product.productQuantity}
 			key={id}
 		/>)
     
