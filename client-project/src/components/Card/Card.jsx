66835930fd1d4e39963b6eb537400013d4e69763
Kenny Jewett <kennethrjewett@gmail.com>
2023-05-22 16:45:32 -0500

    Got POST working for cartDB, but still need to handle quantity

--- client-project/src/components/Card/Card.jsx
+++ client-project/src/components/Card/Card.jsx
@@ -10,7 +10,28 @@ const Card = ({ image, company, product, productType, description, price, quanti
   const handleBuyNow = (product) => {
     console.log(product);    
     dispatch(addOrder(product))
+    const {productId} = product
+    console.log(productId);
+    const cartData = {"productId": productId, "cartQuantity": 2}
+
+    postCart(cartData)
   }
+
+  const postCart = async (data) => {
+    try {
+      const response =  await fetch("http://localhost:3070/api/products/cart", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),        
+      });
+      const result = await response.json();     
+      console.log("Success:", result);
+    }catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className='col-lg-4'>
